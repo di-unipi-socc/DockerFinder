224c339ef6cb5a39d9c8e82e56721cb1a962d3cc
dido18 <davideneri18@gmail.com>
2016-06-16 12:17:12 +0200

    pull only the lastest tag

--- pyFinder/src/pyDescriptor/docker_client.py
+++ pyFinder/src/pyDescriptor/docker_client.py
@@ -1,20 +1,21 @@
 import docker
 
 
-def pull_image(image_name, tag="latest"):
+def pull_image(repo_name, tag="latest"):
     # sets the docker host from your environment variables
     client = docker.Client(**docker.utils.kwargs_from_env(assert_hostname=False))
     # try to set image
-    if not image_name:
+    if not repo_name:
         ims = client.images()
         if len(ims) >= 1:
-            image_name = [im['RepoTags'][0] for im in client.images()][0]
+            repo_name = [im['RepoTags'][0] for im in client.images()][0]
 
-    assert image_name, 'No image given or found locally.'
+    assert repo_name, 'No image given or found locally.'
 
     # get image if not available locally
-    im_names = [im['RepoTags'][0] for im in client.images()]
-    if (not any([image_name in imname for imname in im_names])) and client.search(image_name):
-        print('Image {} not found locally. Pulling from docker hub.'.format(image_name))
-        for line in client.pull(image_name, stream=True):
+    im_names = [im['RepoTags'][0] for im in client.images()] # all the images in the host (first tag)
+
+    if (not any([repo_name in imname for imname in im_names])) and client.search(repo_name): # not found locally and found remote
+        print('Image {} not found locally. Pulling from docker hub.'.format(repo_name))
+        for line in client.pull(repo_name, tag, stream=True):
             print(line.decode())
