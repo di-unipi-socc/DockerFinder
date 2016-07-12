ada29efa72f331e0ec5e3a54c4694f5e0a1b2a6f
dido18 <davideneri18@gmail.com>
2016-07-12 21:56:28 +0200

    webapp is working, after the new branch

---
+++ webapp/app/models/image.js
@@ -0,0 +1,11 @@
+/**
+ * Created by dido on 7/6/16.
+ */
+"use strict";
+var Image = (function () {
+    function Image() {
+    }
+    return Image;
+}());
+exports.Image = Image;
+//# sourceMappingURL=image.js.map
