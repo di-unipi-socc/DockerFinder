8769c339bc50376f5c16f25f29afe8e2706b4198
Davide <davideneri18@gmail>
2016-07-21 15:36:41 +0000

    api/images with count return

--- imagesService/routes/api.js
+++ imagesService/routes/api.js
@@ -7,6 +7,19 @@ var Image = require('../models/image')
 // record all the methods
 Image.methods(['get','put','post','delete']).updateOptions({ new: true });
 
+Image.after('get', function(req, res, next) {
+  //var tmp = res.locals.bundle.title; // Lets swap the title and year fields because we're funny!
+  //res.locals.bundle.title = res.locals.bundle.year;
+  //res.locals.bundle.year = tmp;
+    var count = res.locals.bundle.length
+    res.json({"count": count, "images": res.locals.bundle});
+        console.log("Results " + count)
+
+  next(); // Don't forget to call next!
+});
+
+//Resource.after('recommend', do_something); // Runs after all HTTP verbs
+
 Image.register(router,'/images');
 
 // Return router
