524700af564d15757992ac59e09ab42c352f23b1
dido18 <davideneri18@gmail.com>
2016-08-22 10:11:40 +0000

    remove image in start scanner

--- analysisPhase/start_scanner.sh
+++ analysisPhase/start_scanner.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
 
-# Combining --restart (restart policy) with the --rm (clean up) flag results in an error.
-#docker run -v /var/run/docker.sock:/var/run/docker.sock  --restart=on-failure:5  --net=core-net  dofinder/scanner:latest run
-docker run -v /var/run/docker.sock:/var/run/docker.sock  --restart=on-failure:5  --net=core-net dofinder/scanner:latest run  \
-                                                         --images-url=http://images_server:3000/api/images  \
-                                                         --queue=images --key=images.scan \
-                                                         --software-url=http://software_server:3001/api/software
+
+#docker run -v /var/run/docker.sock:/var/run/docker.sock  --restart=on-failure:5  --net=core-net dofinder/scanner:latest run  \
+#                                                         --images-url=http://images_server:3000/api/images  \
+#                                                         --queue=images --key=images.scan \
+#                                                         --software-url=http://software_server:3001/api/software
+
 #docker run -v /var/run/docker.sock:/var/run/docker.sock  --rm  --net=core-net  dofinder/scanner:latest pull official
+
+
+# TEST SCANNER IAMGES
+ docker run -v /var/run/docker.sock:/var/run/docker.sock  --restart=on-failure:5  --net=core-net dofinder/scanner:latest run  \
+                                                         --images-url=http://images_server:3000/api/images  \
+                                                         --queue=test --key=images.test \
+                                                         --software-url=http://software_server:3001/api/software \
+							 --rmi
+
+
+
+
