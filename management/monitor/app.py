75b9350f59eedd478df34792fadf2f7708de60c4
dido18 <davideneri18@gmail.com>
2016-11-11 11:32:03 +0100

    management folder with monitor service written in flask

---
+++ management/monitor/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, Response,request, jsonify, url_for
+import pika
+import sys
+import os
+import socket
+
+app = Flask(__name__)
+
+# Enable debugging if the DEBUG environment variable is set and starts with Y
+# app.debug = os.environ.get("DEBUG", "").lower().startswith('y')
+#
+hostname = socket.gethostname()
+#
+# urandom = os.open("/dev/urandom", os.O_RDONLY)
+
+@app.route('/')
+def index():
+    url = ""
+    with app.test_request_context():
+        url = url_for('service', servicename="rabbitmq", queuename="images")
+    return url
+
+
+@app.route('/service/<servicename>/queue/<queuename>')
+def service(servicename, queuename):
+    try:
+        msg_count = count_queue_msg(servicename,queuename)
+        return jsonify(
+                err=False,
+                service=servicename,
+                load=msg_count,
+                queue=queuename
+                )
+    except :
+        #err_msg=
+        return jsonify(
+                err=True,
+                msg="Error in connecting"+servicename
+                )
+
+
+
+def count_queue_msg(service, queue):
+
+    url = "amqp://guest:guest@"+service+":5672"
+    print("connecting to : " +url)
+    connection = pika.BlockingConnection(pika.URLParameters(url))
+    # Open the channel
+    channel = connection.channel()
+    # Declare the queue
+    queue =  channel.queue_declare(queue=queue,
+                    passive=True,
+                    durable=True,
+                    exclusive=False,
+                    auto_delete=False
+                    )
+
+    c = queue.method.message_count
+    return c
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3002)
---
+++ management/scaleScanner.py
@@ -0,0 +1,81 @@
+import requests
+import time
+import subprocess
+#from subprocess import Popen, PIPE
+
+
+class ScaleScanner:
+
+    def __init__(self, update_interval, swarm=True):
+        self.update_interval = update_interval
+        self._swarm_mode=swarm
+        self._rabbitUrl = "http://0.0.0.0:3002/service/rabbitmq/queue/images"
+
+    def run_loop(self, service):
+        while(True):
+            time.sleep(self.update_interval)
+            # count the message in the queue
+            res = requests.get(self._rabbitUrl)
+
+            if res.status_code == requests.codes.ok:
+                json_response = res.json()
+                print(json_response)
+                if(not json_response['err']):
+                    count_msg=json_response['load']
+                    print(str(count_msg)+ ": number of msgs in the queue")
+                    scale = 1
+                    if count_msg < 100:
+                        scale = 5
+                    elif count_msg < 500:
+                        scale = 10
+                    elif count_msg < 1000:
+                        scale = 30
+                    else:
+                        scale = 40
+                    #scale the scanners
+                    self.scale_service(service, scale)
+
+                    print("Scaled to "+ service + "="+str(scale))
+                else:
+                    print(json_response['msg'])
+
+    def scale_service(self, service, scale):
+        if(self._swarm_mode):
+            self.scale_swarm(service, scale)
+        else:
+            self.scale_compose(service, scale)
+
+
+    def scale_compose(self, service_name, scale):
+        print("scaling compose")
+
+        r = subprocess.run(["docker-compose scale "+ service_name+"="+str(scale)],
+        #"docker-compose", "scale",  service_name+"="+str(scale) ],
+                        shell=True,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
+        print(r.stderr.decode("utf-8"))
+
+    def scale_swarm(self, service_name, scale):
+        print("scaling swarm")
+        r = subprocess.run(["docker", "service", "scale",  service_name+"="+str(scale)],
+                        shell=True,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
+        print(r)
+
+
+if __name__ == '__main__':
+
+    scaling = ScaleScanner(5, swarm=False)  # rabbitmq
+
+    while True:
+        try:
+            scaling.run_loop("scanner")
+        except Exception as e:
+            print (e)
+            print("Waiting 10s and restarting.")
+            time.sleep(10)
+
+
+    #update_service with mode={'Replicated': {'Replicas': n}}.
