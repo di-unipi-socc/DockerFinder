796dc917b9a1d10feadb0c7f4b4bf034911a300a
dido18 <davideneri18@gmail.com>
2017-01-16 15:38:02 +0100

    list of images as single row, checker verify for update the images description

--- discovery/webapp/public/app/componenets/images.component.ts
+++ discovery/webapp/public/app/componenets/images.component.ts
@@ -15,16 +15,20 @@ import {ImageService} from "../services/image.service";
     template: `
         <button class="btn btn-primary"  (click)="goBack()">Back</button>
 
-        <div class="container">
+        <!--div class="container">
 
-          <div class="row">  <!--inti row-->
-            <div class="col-sm-6 col-md-4" *ngFor="let image of images"  [class.selected]="image === selectedImage" (click)="onSelect(image)">
+        <!- -inti row "col-sm-6 col-md-4"- ->
+          <div class="row">
+            <div class="col-xs-12" *ngFor="let image of images"  [class.selected]="image === selectedImage" (click)="onSelect(image)">
+
               <div class="thumbnail">
-                <img src="app/images/docker.png" style="width:70px" alt="">
-
+              <span>
+                <img src="app/images/docker.png" style="width:50px" alt="">
+                <p class="reponame"  (click)="onSelect(image)"> {{image.name}} </p>
+              </span>
                 <div class="caption">
-                  <p class="reponame" (click)="onSelect(image)"> {{image.name}} </p>
-                  <!--h4 class="reponame" (click)="onSelect(image)">{{image.name}} </h4-->
+
+                  <!- -h4 class="reponame" (click)="onSelect(image)">{{image.name}} </h4- ->
                   <div class="details-image">
                     <span class="glyphicon glyphicon-star" aria-hidden="true"> </span> {{image.stars}} Star
                     <span class="glyphicon glyphicon-download" aria-hidden="true"></span>  {{image.pulls}}  Pulls
@@ -32,8 +36,8 @@ import {ImageService} from "../services/image.service";
                   </div>
 
                   <div class="group-softwares">
-                    <div class="row">  <!--nested row-->
-                        <div class="col-md-4 col-sm-3 my-software"  *ngFor="let sw of image.softwares">
+                    <div class="row">  <!- -nested row- ->
+                        <div class="col-md-3 col-sm-3 my-software"  *ngFor="let sw of image.softwares">
                            {{sw.software}} {{sw.ver}}
                         </div>
                     </div>
@@ -41,8 +45,32 @@ import {ImageService} from "../services/image.service";
                 </div>
               </div>
             </div>
-          </div>  <!--end row-->
+          </div>  <!- -end row- ->
+        </div-->
+<!-- container fluid example-->
+    <div  class="container-fluid">
+        <div class="row-image" *ngFor="let image of images"  (click)="onSelect(image)">
+
+            <img src="app/images/docker.png" style="width:70px" class="row-image-group" alt="">
+
+            <div class="row-image-group">
+              <h1 class="reponame"  (click)="onSelect(image)"> {{image.name}} </h1>
+              <span class="glyphicon glyphicon-star" aria-hidden="true"> </span> {{image.stars}} Star
+              <span class="glyphicon glyphicon-download" aria-hidden="true"></span>  {{image.pulls}}  Pulls
+              <span class="glyphicon glyphicon-save" aria-hidden="true"> </span>  {{image.size | toMegabytes }}MB
+              <div [hidden]="image.is_automated">automated build</div>
+              <div> </div>
+              <!--div ng-show="image.is_automated">Is automated</div-->
+              <!--span [hidden]="!image.is_private">Public</span-->
+
+            <div class="my-software row-image-group"  *ngFor="let sw of image.softwares">
+                   {{sw.software}} {{sw.ver}}
+            </div>
+          </div>
+
         </div>
+    </div>
+
     `,
 
 
@@ -85,7 +113,15 @@ export class ImagesComponent implements OnInit {
     // }
 
     onSelect(image: Image) {
-        this.selectedImage = image;
+        //this.selectedImage = image;
+      //  name =
+        var values = image.name.split(':');
+        var repository = values[0];
+        var tag = values[1];
+        //console.log(repository)
+        var url = "https://hub.docker.com/r/"+repository
+
+        window.open(url)
     }
 
     gotoDetail() {
