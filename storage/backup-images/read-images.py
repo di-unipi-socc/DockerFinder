4b2ee4c528e2e4fa698319064dbe8aba63b9b650
dido <davide.neri@unipi.it>
2018-02-16 10:47:56 +0100

    immages manager save with date and count into json

--- storage/backup-images/read-images.py
+++ storage/backup-images/read-images.py
@@ -9,11 +9,10 @@ if (args.file):
     print("Reading JSON: {} ...".format(f))
     with open(f,encoding="utf8") as json_data:
        	data = json.load(json_data)
-	# number of images stored into the JSOn file
+     	# number of images stored into the JSOn file
         total_images = data['count']
         print( "{} total images in the file".format(total_images))
         # array of images descriptions
         images = data['images']
         #for i in images:
         #    print("Name: {}".format(i['name']))
-
